c4c8eb5c6652b613149dc9bc9e82e3aa363db9e4
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-12-15 20:45:06 -0500

    added queries

--- api/server.js
+++ api/server.js
@@ -6,6 +6,6 @@ const db = require("../data/dbConfig.js");
 const server = express();
 
 server.use(express.json());
-server.use("./accounts", accountRouter)
+server.use("/accounts", accountRouter)
 
 module.exports = server;
